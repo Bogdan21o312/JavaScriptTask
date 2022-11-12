5d448b1b97ec380c9cb865399a61712f14193e14
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-12 23:21:04 +0200

    Condition task

---
+++ JavaScriptTask/NumberOfIslands/index.js
@@ -0,0 +1,14 @@
+// NumberOfIslands ====================================================================================================
+
+/**
+ *
+ *  It is necessary to count the number of islands in the matrix.
+ *  An island is considered to be units (1)
+ *  that are next to each other horizontally and vertically.
+ *  Matrix cells with zeros are considered water.
+ *
+ */
+
+
+
+//====================================================================================================
