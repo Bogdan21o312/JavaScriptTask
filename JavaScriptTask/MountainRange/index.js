a034efd5c82b9b42324a0159728a705b217e8646
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-15 16:29:40 +0200

    Start

---
+++ JavaScriptTask/MountainRange/index.js
@@ -0,0 +1,28 @@
+// MountainRange ====================================================================================================
+
+/**
+ *
+ * A mountain range is an array that can be conditionally
+ * divided into 2 parts - increasing and decreasing,
+ * and at the junction of these two parts there is a peak that we need to find.
+ * In the ascending part,
+ * the numbers are sorted in ascending order.
+ * And in descending order - in descending order.
+ * In this case, there may be a situation where the increasing or decreasing part is completely absent.
+ * And the highest point (peak) will still be present.
+ *
+ */
+
+let arr1 = [1, 2, 3, 4, 3, 2, 1];
+let arr3 = [10, 9, 8, 7, 6];
+let arr2 = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2];
+
+let peakIndexInMountainArray = (arr) => {
+
+}
+
+console.log(peakIndexInMountainArray(arr1));
+console.log(peakIndexInMountainArray(arr2));
+console.log(peakIndexInMountainArray(arr3));
+
+//====================================================================================================
